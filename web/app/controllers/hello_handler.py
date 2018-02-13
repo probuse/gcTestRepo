7a8a104e236906ec1e8538a0ec54b034e810963a
probuse <nprobuse@gmail.com>
2018-02-13 13:25:15 +0300

    Added Hello Class, a form in hello.html

--- web/app/controllers/hello_handler.py
+++ web/app/controllers/hello_handler.py
@@ -1,12 +1,20 @@
 import web
-
+from . import render
 
 class Hello:
     
     def GET(self):
         "Controls get requests to hello url"
-        pass
+        name = 'Test Site'
+        l = locals()
+        del l['self']
+        return render.hello(**l)
 
     def POST(self):
         "Controls post requests to hello url"
-        pass
+        result = web.input()
+        name = result.myText
+        print name
+        l = locals()
+        del l['self']
+        return render.hello(**l) 
